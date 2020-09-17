f5709d68dfd647ee2fb8094f1b3fbc5604fabf32
MenSeb <sebastien.menard.github@gmail.com>
2020-09-17 16:22:59 -0400

    install & use router

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 export default function App() {
-    return null;
+    return (
+        <BrowserRouter>
+            <header></header>
+
+            <main></main>
+
+            <footer></footer>
+        </BrowserRouter>
+    );
 }
