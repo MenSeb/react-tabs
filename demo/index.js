5b672bd1f54d0e0cf8886370d6da380a8af53cca
MenSeb <sebastien.menard.github@gmail.com>
2021-01-03 01:54:45 -0500

    update demo to fit package

--- demo/index.js
+++ demo/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Tabs } from '~';
+import { Panel, PanelList, Tab, TabList, Tabs, TabsProvider } from '../src';
+import './styles.scss';
 
 function Demo() {
-    return <Tabs id="tabs" />;
+    return (
+        <>
+            <TabsProvider
+                automatic={false}
+                label="select a tab"
+                removable={true}
+                optimise={false}
+                orientation="vertical"
+                override={true}
+                tab={0}
+                tabs={5}
+            >
+                <Tabs>
+                    <TabList>
+                        <Tab removable={false}>Tab 1</Tab>
+                        <Tab>Tab 2</Tab>
+                        <Tab>Tab 3</Tab>
+                        <Tab>Tab 4</Tab>
+                        <Tab>Tab 5</Tab>
+                    </TabList>
+                    <PanelList>
+                        <Panel>Panel 1</Panel>
+                        <Panel>Panel 2</Panel>
+                        <Panel>Panel 3</Panel>
+                        <Panel>Panel 4</Panel>
+                        <Panel>Panel 5</Panel>
+                    </PanelList>
+                </Tabs>
+            </TabsProvider>
+        </>
+    );
 }
 
 render(<Demo />, document.getElementById('root'));
