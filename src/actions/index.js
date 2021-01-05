545a3de173785646090c6de45a84865877ff7480
MenSeb <sebastien.menard.github@gmail.com>
2021-01-05 15:45:46 -0500

    export actions

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+export { default as first } from './first';
+export { default as last } from './last';
+export { default as next } from './next';
+export { default as prev } from './prev';
+export { default as register } from './register';
+export { default as remove } from './remove';
+export { default as select } from './select';
