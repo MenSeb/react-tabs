fc76e22f863377c3735f2bfa200226cc4c892afc
MenSeb <sebastien.menard.github@gmail.com>
2021-01-08 18:57:59 -0500

    replace key codes with special chars from user-event

--- test/interaction.test.js
+++ test/interaction.test.js
@@ -1,5 +1,5 @@
 import { fireEvent } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import userEvent, { specialChars } from '@testing-library/user-event';
 import { ORIENTATION_VERTICAL as orientation } from 'utilities';
 import { getPanel, getTab, renderTabs, tab, tabs } from './';
 
@@ -43,7 +43,7 @@ describe('Keyboard interaction', () => {
         });
     });
 
-    ['enter', 'space'].forEach((key) => {
+    [specialChars.enter, specialChars.space].forEach((key) => {
         describe(`When user press ${key}`, () => {
             it('activates the associated tab panel', () => {
                 renderTabs({ props: { automatic: false } });
@@ -65,7 +65,7 @@ describe('Keyboard interaction', () => {
 
             expect(getTab()).not.toHaveFocus();
 
-            userEvent.type(getTab(tab), '{home}');
+            userEvent.type(getTab(tab), specialChars.home);
 
             expect(getTab()).toHaveFocus();
         });
@@ -79,7 +79,7 @@ describe('Keyboard interaction', () => {
 
             expect(getTab(tabs - 1)).not.toHaveFocus();
 
-            userEvent.type(getTab(), '{end}');
+            userEvent.type(getTab(), specialChars.end);
 
             expect(getTab(tabs - 1)).toHaveFocus();
         });
@@ -92,7 +92,7 @@ describe('Keyboard interaction', () => {
             const currentTab = getTab(tab);
             const currentPanel = getPanel(tab);
 
-            userEvent.type(currentTab, '{del}');
+            userEvent.type(currentTab, specialChars.delete);
 
             expect(currentTab).not.toBeInTheDocument();
             expect(currentPanel).not.toBeInTheDocument();
@@ -102,7 +102,7 @@ describe('Keyboard interaction', () => {
             const currentTab = getTab(tab);
             const nextTab = getTab(tab + 1);
 
-            userEvent.type(currentTab, '{del}');
+            userEvent.type(currentTab, specialChars.delete);
 
             expect(nextTab).toHaveFocus();
         });
@@ -111,14 +111,14 @@ describe('Keyboard interaction', () => {
             const currentTab = getTab(tabs - 1);
             const nextTab = getTab(tabs - 2);
 
-            userEvent.type(currentTab, '{del}');
+            userEvent.type(currentTab, specialChars.delete);
 
             expect(nextTab).toHaveFocus();
         });
 
         it('moves focus to the next element in the sequence, if the last remaining tab was deleted', () => {
             for (let i = 0; i < tabs; i++) {
-                userEvent.type(getTab(), '{del}');
+                userEvent.type(getTab(), specialChars.delete);
             }
 
             expect(document.body).toHaveFocus();
@@ -130,13 +130,13 @@ describe('Keyboard interaction', () => {
             beforeEach(() => renderTabs());
 
             it('moves focus to the previous tab', () => {
-                userEvent.type(getTab(tab), '{arrowleft}');
+                userEvent.type(getTab(tab), specialChars.arrowLeft);
 
                 expect(getTab(tab - 1)).toHaveFocus();
             });
 
             it('moves focus to the last tab, if focus is on the first tab', () => {
-                userEvent.type(getTab(), '{arrowleft}');
+                userEvent.type(getTab(), specialChars.arrowLeft);
 
                 expect(getTab(tabs - 1)).toHaveFocus();
             });
@@ -146,7 +146,7 @@ describe('Keyboard interaction', () => {
             it('keeps focus on the current tab', () => {
                 renderTabs({ props: { orientation } });
 
-                userEvent.type(getTab(tab), '{arrowleft}');
+                userEvent.type(getTab(tab), specialChars.arrowLeft);
 
                 expect(getTab(tab)).toHaveFocus();
             });
@@ -158,13 +158,13 @@ describe('Keyboard interaction', () => {
             beforeEach(() => renderTabs());
 
             it('moves focus to the next tab', () => {
-                userEvent.type(getTab(tab), '{arrowright}');
+                userEvent.type(getTab(tab), specialChars.arrowRight);
 
                 expect(getTab(tab + 1)).toHaveFocus();
             });
 
             it('moves focus to the first tab, if focus is on the last tab', () => {
-                userEvent.type(getTab(tabs - 1), '{arrowright}');
+                userEvent.type(getTab(tabs - 1), specialChars.arrowRight);
 
                 expect(getTab()).toHaveFocus();
             });
@@ -174,7 +174,7 @@ describe('Keyboard interaction', () => {
             it('keeps focus on the current tab', () => {
                 renderTabs({ props: { orientation } });
 
-                userEvent.type(getTab(tab), '{arrowright}');
+                userEvent.type(getTab(tab), specialChars.arrowRight);
 
                 expect(getTab(tab)).toHaveFocus();
             });
@@ -186,13 +186,13 @@ describe('Keyboard interaction', () => {
             beforeEach(() => renderTabs({ props: { orientation } }));
 
             it('moves focus to the previous tab', () => {
-                userEvent.type(getTab(tab), '{arrowup}');
+                userEvent.type(getTab(tab), specialChars.arrowUp);
 
                 expect(getTab(tab - 1)).toHaveFocus();
             });
 
             it('moves focus to the last tab, if focus is on the first tab', () => {
-                userEvent.type(getTab(), '{arrowup}');
+                userEvent.type(getTab(), specialChars.arrowUp);
 
                 expect(getTab(tabs - 1)).toHaveFocus();
             });
@@ -202,7 +202,7 @@ describe('Keyboard interaction', () => {
             it('keeps focus on the current tab', () => {
                 renderTabs();
 
-                userEvent.type(getTab(tab), '{arrowup}');
+                userEvent.type(getTab(tab), specialChars.arrowUp);
 
                 expect(getTab(tab)).toHaveFocus();
             });
@@ -214,13 +214,13 @@ describe('Keyboard interaction', () => {
             beforeEach(() => renderTabs({ props: { orientation } }));
 
             it('moves focus to the next tab', () => {
-                userEvent.type(getTab(tab), '{arrowdown}');
+                userEvent.type(getTab(tab), specialChars.arrowDown);
 
                 expect(getTab(tab + 1)).toHaveFocus();
             });
 
             it('moves focus to the first tab, if focus is on the last tab', () => {
-                userEvent.type(getTab(tabs - 1), '{arrowdown}');
+                userEvent.type(getTab(tabs - 1), specialChars.arrowDown);
 
                 expect(getTab()).toHaveFocus();
             });
@@ -230,7 +230,7 @@ describe('Keyboard interaction', () => {
             it('keeps focus on the current tab', () => {
                 renderTabs();
 
-                userEvent.type(getTab(tab), '{arrowdown}');
+                userEvent.type(getTab(tab), specialChars.arrowDown);
 
                 expect(getTab(tab)).toHaveFocus();
             });
