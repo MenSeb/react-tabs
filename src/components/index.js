f8ba9003502f31c6e2100c2fbaabd52aa3ed0ce5
MenSeb <sebastien.menard.github@gmail.com>
2021-01-08 15:04:52 -0500

    export components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
-export { default as Tab } from './tab';
-export { default as TabList } from './tablist';
-export { default as TabPanel } from './tabpanel';
-export { default as Tabs } from './tabs';
+export { default as Panel } from './Panel';
+export { default as PanelList } from './PanelList';
+export { default as Tab } from './Tab';
+export { default as TabList } from './TabList';
+export { default as Tabs } from './Tabs';
+export { default as TabsProvider } from './TabsProvider';
