a56971873ff05318aa683d47dd67a40f6e94bb16
MenSeb <sebastien.menard.github@gmail.com>
2021-01-09 11:41:51 -0500

    update webpack configs

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,9 +5,31 @@ const path = require('path');
 module.exports = merge(config, {
     devtool: 'source-map',
     entry: path.resolve(__dirname, 'src/index.js'),
+    externals: {
+        'prop-types': {
+            amd: 'prop-types',
+            commonjs: 'prop-types',
+            commonjs2: 'prop-types',
+            root: 'PropTypes',
+        },
+        react: {
+            amd: 'react',
+            commonjs: 'react',
+            commonjs2: 'react',
+            root: 'React',
+        },
+        'react-dom': {
+            amd: 'react-dom',
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            root: 'ReactDOM',
+        },
+    },
     mode: 'production',
     output: {
         filename: 'index.js',
+        library: 'ReactBoilerplate',
+        libraryTarget: 'umd',
         path: path.resolve(__dirname, 'dist'),
     },
 });
