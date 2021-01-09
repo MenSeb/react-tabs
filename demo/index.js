9ac47239e9c8c3e450e65d7c67f32fa6015153cc
MenSeb <sebastien.menard.github@gmail.com>
2021-01-09 11:40:19 -0500

    update demo files

--- demo/index.js
+++ demo/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from '~';
+import App from '../src';
+import './styles.scss';
 
 render(<App />, document.getElementById('root'));
