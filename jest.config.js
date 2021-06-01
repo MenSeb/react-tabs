4c370cb64e78acc2be4de9580d5d7f1eff6dcb5e
Sébastien Menard <sebastien.menard.github@gmail.com>
2021-06-01 09:34:49 -0400

    [Enhancement] Merge boilerplate (#120)
    * Pin dependencies (#73)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * update jest config with jsdom test environment (#77)
    
    * create script reset (#78)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
     moduleDirectories: ['node_modules', 'src'],
     setupFiles: ['./jest.setup.js'],
     setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+    testEnvironment: 'jsdom',
     verbose: true,
 };
